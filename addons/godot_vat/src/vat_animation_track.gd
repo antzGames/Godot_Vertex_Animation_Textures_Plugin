4693a2e87b4446c3b85d9a8674355b74f3bc50e8
Antz <Antz.Games@outlook.com>
2026-08-12 09:20:26 -0400

    Version 0.1.4

--- addons/godot_vat/src/vat_animation_track.gd
+++ addons/godot_vat/src/vat_animation_track.gd
@@ -1,10 +1,25 @@
 class_name VATAnimationTrack
 extends Resource
 
+## Name of Animation Track.[br]
+## If empty, a track number is assigned automatically.
 @export var name: String
-@export_range(0, 8192) var startFrame: int
-@export_range(0, 8192) var endFrame: int
+
+## Start frame of animation track.[br]
+## Must be between [0..8191].
+@export_range(0, 8191) var startFrame: int
+
+## End frame of animation track.[br]
+## Must be between [0..8191].[br]
+## Should be equal or greater than start frame.
+@export_range(0, 8191) var endFrame: int
+
+## Is the animation track only to be played once,[br]
+## like a death animation, or does it loop?
 @export var isLooping: bool = true
+
+## Each animation track can have it own unique fps.[br]
+## If set to 0, then it will be replaced by [member VATMultiMeshInstance3D.default_fps].
 @export_range(0, 120) var framerate: int
 
 func set_track(name_in: String, start_in: int, end_in: int, framerate_in: int, loop: bool):
